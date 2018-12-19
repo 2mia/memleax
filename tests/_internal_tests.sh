812b1f00db5dcd10e32e77792ffc10a8a61f4949
Mihai <miancu@adobe.com>
2018-12-19 05:21:10 +0200

    WIP: ubuntu

--- tests/_internal_tests.sh
+++ tests/_internal_tests.sh
@@ -1,16 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 cd "$(dirname "$0")"
 
 set -e
 
-make 01
+gcc 01.c -lpthread -o 01 && strip 01
 ./01 & 
 TID=$!
 sleep 0.1 #give time to start
 
 ../build/memleax -e 1 $TID &
 sleep 1 #give time to attach
-(echo sending sighup ...; kill -SIGUSR1 $TID)
+echo sending sighup ...
+kill -SIGUSR1 $TID
 
 wait $TIP
 wait 
